fa797f28d5bd04a164d6a162e8625bf05e69f628
iSqmu <82478445+iSqmu@users.noreply.github.com>
2024-05-20 20:20:54 -0500

    Add files via upload

--- scripts/404.js
+++ scripts/404.js
@@ -1,23 +1,44 @@
-//! toggle dark mode 
+//! toggle dark mode
 
 let btndark = document.querySelector('.sun');
-let mode = document.querySelector('.mode')
-let isdark = false;
+let mode = document.querySelector('.mode');
+let isdark;
 
-btndark.addEventListener('click', ()=>{
-    if(!isdark){
-        btndark.style.transform = 'translateX(200%)'
-        btndark.innerHTML = '<i class="fa-solid fa-moon"></i>'
-    btndark.style.background = 'black'
-        isdark = true;
-        mode.innerHTML = 'Dark Mode'
-        mode.style.transform = 'translateX(-60%)'
-    } else {
-        btndark.style.transform = 'translateX(0)'
-        btndark.innerHTML = '<i class="fa-solid fa-sun"></i>'
-        btndark.style.background = '#33D4FF'
-        isdark = false;
-        mode.innerHTML = 'Clear Mode'
-        mode.style.transform = 'translateX(5%)'
-    }
-})
+btndark.addEventListener('click', () => {
+	if (isdark === false) {
+		btndark.style.transform = 'translateX(200%)';
+		btndark.innerHTML = '<i class="fa-solid fa-moon"></i>';
+		btndark.style.background = 'black';
+		isdark = true;
+		mode.innerHTML = 'Dark Mode';
+		mode.style.transform = 'translateX(-60%)';
+
+		document.querySelector('.go-back a').style.color = '#eee';
+	} else if (isdark === 'true') {
+		btndark.style.transform = 'translateX(0)';
+		btndark.innerHTML = '<i class="fa-solid fa-sun"></i>';
+		btndark.style.background = '#33D4FF';
+		isdark = false;
+		mode.innerHTML = 'Clear Mode';
+		mode.style.transform = 'translateX(5%)';
+		document.querySelector('.go-back a').style.color = '#eee';
+	}
+	document.querySelector('body').classList.toggle('dark-mode');
+	localStorage.setItem('dark-mode', isdark);
+});
+
+if (localStorage.getItem('dark-mode') === 'true') {
+	document.querySelector('body').classList.add('dark-mode');
+	btndark.style.transform = 'translateX(200%)';
+	btndark.innerHTML = '<i class="fa-solid fa-moon"></i>';
+	btndark.style.background = 'black';
+	isdark = true;
+	mode.innerHTML = 'Dark Mode';
+	mode.style.transform = 'translateX(-60%)';
+}
+
+if (!localStorage.getItem('dark-mode')) {
+	isdark = false;
+} else {
+	isdark = localStorage.getItem('dark-mode');
+}
